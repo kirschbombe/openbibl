1575f0f97e8437f67aa8a6a31b5ddf7d00031d96
Nathan Day <rnathanday@yahoo.com>
2014-05-26 15:23:07 -0500

    removed copied spec config

--- tests/js/spec/openbibl/download.js
+++ tests/js/spec/openbibl/download.js
@@ -3,52 +3,19 @@ define(
 , function(module, $, jj, obpev, browse, model, obpstate) {
 
     describe(module.id, function(){
-
-        describe('load/initialization', function() {
-            it('load', function(){
-                expect(browse).toBeDefined();
-            }); // it    
-            it('initialized state', function() {
-                expect(browse.filter_indices()).toEqual([]);
-                expect(browse.highlight_items()).toEqual(
-                    { element : [] }
-                );
-            }); // it
-        }); // describe
-    
         describe('click events', function() {
             beforeEach(function() {
                 loadFixtures('otherEurope.html');
                 browse.init();
                 spyOn(obpev, 'raise');
             });
-            it('.obp-browse-clear', function() {
-                 var spyEvent = spyOnEvent('.obp-browse-clear', 'click');
-                 $('.obp-browse-clear').first().click();
-                 expect('click').toHaveBeenTriggeredOn('.obp-browse-clear');
+            it('.', function() {
+                 var spyEvent = spyOnEvent('.', 'click');
+                 $('.').first().click();
+                 expect('click').toHaveBeenTriggeredOn('.');
                  expect(spyEvent).toHaveBeenTriggered();
-                 expect(obpev.raise).toHaveBeenCalledWith('obp:browse-term-change', 'browse');
-                 expect(obpev.raise).toHaveBeenCalledWith('obp:filter-change', 'browse');
-            }); // it
-            it('.obp-browse-checkbox', function() { 
-                 var spyEvent = spyOnEvent('.obp-browse-checkbox', 'click');
-                 $('.obp-browse-checkbox').first().click();
-                 expect('click').toHaveBeenTriggeredOn('.obp-browse-checkbox');
-                 expect(spyEvent).toHaveBeenTriggered();
-                 expect(obpev.raise).toHaveBeenCalledWith('obp:browse-term-change', 'browse');
-                 expect(obpev.raise).toHaveBeenCalledWith('obp:filter-change', 'browse');
             }); // it
         }); // describe
         
-        describe('model messaging', function(){
-            it('model called to update query data', function() {
-                browse.init();
-                obpstate.query.data = { browse: {} };
-                spyOn(model, 'handle_query_data');
-                browse.handle_query_data();
-                expect(model.handle_query_data).toHaveBeenCalled();
-            }); // it
-        }); // describe
-    
     });
 });
