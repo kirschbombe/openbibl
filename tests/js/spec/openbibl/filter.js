43b4c4ed94f8f862f122556aec2a29314ebcc50a
Nathan Day <rnathanday@yahoo.com>
2014-05-31 19:32:12 -0500

    added test to openbibl/filter to confirm browse-item onClick behavior

--- tests/js/spec/openbibl/filter.js
+++ tests/js/spec/openbibl/filter.js
@@ -1,6 +1,6 @@
 define(
-  ['module', 'jquery', 'jasmine-jquery', 'obpev', 'filter']
-, function(module, $, jj, obpev, filter) {
+  ['module', 'jquery', 'jasmine-jquery', 'obpev', 'filter', 'browse']
+, function(module, $, jj, obpev, filter, browse) {
 
     describe(module.id, function(){
 
@@ -18,11 +18,8 @@ define(
         }); // describe
 
         describe('subscribed sources', function(){
-            beforeEach(function() {
-                // loadFixtures('otherEurope.full.html');
-                obpev.init();
-            });
             it('Subscribed source filter_indices() method called on obp:filter-change event', function(){
+                obpev.init();
                 var source = {
                     filter_indices : function() { return []; }
                 };
@@ -31,10 +28,43 @@ define(
                 obpev.raise('obp:filter-change', module.id);
                 expect(source.filter_indices).toHaveBeenCalled();
             }); // it
-/*            it('requested filter operation occurred', function(){
-
+            it('Click event triggered expected filter query', function(){
+                loadFixtures('otherEurope.full.html');
+                browse.init();
+                var source = {
+                    filter_indices : function() {}
+                };
+                spyOn(source,'filter_indices');
+                filter.init([source]);
+                $('.obp-browse-checkbox').first().click();
+                expect(source.filter_indices).toHaveBeenCalled();
             }); // it
-*/
+            it('Click event results in expected number/index of entries', function(){
+                var obpstate = require('obpstate');
+                obpstate.rebase(window.obp.obpstate);
+                loadFixtures('otherEurope.full.html');
+                browse.init();
+                var indices = [0];
+                var source = {
+                    filter_indices : function() {
+                        return indices;
+                    }
+                };
+                filter.init([source]);
+                $('.obp-browse-checkbox').first().click();
+                // make sure we have the same number of div.entry
+                // elements as indicated in 'indices' above
+                expect($('.entry:visible').length).toEqual(indices.length);
+                // make sure we have the entries with the indices given above
+                indices.forEach(function(ind){
+                    var selector = '.entry:visible'
+                                 + '[data-src-index='
+                                 + ind.toString()
+                                 + ']';
+                    expect($(selector)).not.toBe([]);
+                });
+            });
+
         }); // describe
     });
 });
