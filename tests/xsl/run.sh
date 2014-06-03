e454181ef7e0a374b778063dc34d32da63b37bf6
Nathan Day <rnathanday@yahoo.com>
2014-06-03 08:18:28 -0500

    added xsl test for browe/search json data file

--- tests/xsl/run.sh
+++ tests/xsl/run.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 #
 java net.sf.saxon.Transform -s:otherEurope.xml -xsl:../../xsl/openbibl.boot.xsl  -o:otherEurope.boot.html
-java net.sf.saxon.Transform -s:otherEurope.xml -xsl:../../xsl/openbibl.xsl  -o:otherEurope.bibl.html
-
+java net.sf.saxon.Transform -s:otherEurope.xml -xsl:../../xsl/openbibl.xsl       -o:otherEurope.bibl.html
+java net.sf.saxon.Transform -s:otherEurope.xml -xsl:../../xsl/openbibl.query.xsl -o:otherEurope.query.json
+git diff .
