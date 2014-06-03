6c66fcde6277f540e32d4aee72f5abe1f9cd536f
Nathan Day <rnathanday@yahoo.com>
2014-06-03 09:05:44 -0500

    removed unused xsl/openbibl.shared.xsl stylesheet
    removed unused xsl/openbibl.shared.xsl stylesheet, which was used
    earlier in the boot.xsl and openbibl.xsl stylesheets but is no
    longer needed

--- tests/xsl/run.sh
+++ tests/xsl/run.sh
@@ -3,4 +3,7 @@
 java net.sf.saxon.Transform -s:otherEurope.xml -xsl:../../xsl/openbibl.boot.xsl  -o:otherEurope.boot.html
 java net.sf.saxon.Transform -s:otherEurope.xml -xsl:../../xsl/openbibl.xsl       -o:otherEurope.bibl.html
 java net.sf.saxon.Transform -s:otherEurope.xml -xsl:../../xsl/openbibl.query.xsl -o:otherEurope.query.json
+
+java net.sf.saxon.Transform -s:otherEurope.xml -xsl:../../xsl/openbibl.lod.xsl   -o:otherEurope.query.json
+
 git diff .
