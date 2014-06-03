96ecc7e132543f2144ff87f17bddaa8144b9866a
Nathan Day <rnathanday@yahoo.com>
2014-06-03 13:30:09 -0500

    updated tests/xsl/run.sh
    Updated shell script for xsl tests to include linked-open-data
    stylesheet.

--- tests/xsl/run.sh
+++ tests/xsl/run.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 #
-java net.sf.saxon.Transform -s:otherEurope.xml -xsl:../../xsl/openbibl.boot.xsl  -o:otherEurope.boot.html
-java net.sf.saxon.Transform -s:otherEurope.xml -xsl:../../xsl/openbibl.xsl       -o:otherEurope.bibl.html
-java net.sf.saxon.Transform -s:otherEurope.xml -xsl:../../xsl/openbibl.query.xsl -o:otherEurope.query.json
 
-java net.sf.saxon.Transform -s:otherEurope.xml -xsl:../../xsl/openbibl.lod.xsl   -o:otherEurope.query.json
+OEUR="../../examples/otherEurope/otherEurope.xml"
+TEMPL="../../TEI/DescTemplate.xml"
 
-git diff .
+java net.sf.saxon.Transform -s:"$OEUR"  -xsl:../../xsl/openbibl.boot.xsl  -o:otherEurope.boot.html
+java net.sf.saxon.Transform -s:"$OEUR"  -xsl:../../xsl/openbibl.xsl       -o:otherEurope.bibl.html
+java net.sf.saxon.Transform -s:"$OEUR"  -xsl:../../xsl/openbibl.query.xsl -o:otherEurope.query.json
+java net.sf.saxon.Transform -s:"$TEMPL" -xsl:../../xsl/openbibl.lod.xsl   -o:DescTemplate.lod.xml
